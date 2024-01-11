1bbc6c29ecee3b621c8b063997b4bb56babaad51
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-01-11 15:48:04 +0800

    Table Component Created

---
+++ frontend/components/TableComponent.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+class TableComponent extends Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+    
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+
+}
+
+export default TableComponent
